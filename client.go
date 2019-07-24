0ae6081e975bdf3f9e6cc4929123a34e597d7119
AkiraFukushima <h3.poteto@gmail.com>
2019-07-24 18:46:00 +0900

    Divide client module

---
+++ client.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/h3poteto/helm/pkg/helm"
+	"github.com/h3poteto/helm/pkg/helm/portforwarder"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/helm/pkg/kube"
+)
+
+func NewClient() (*helm.Client, error) {
+	kubeconfig := os.Getenv("KUBECONFIG")
+	if len(kubeconfig) == 0 {
+		kubeconfig = "~/.kube/config"
+	}
+
+	config, client, err := getKubeClient("", kubeconfig)
+	if err != nil {
+		return nil, err
+	}
+	tillerTunnel, err := portforwarder.New("kube-system", client, config)
+	if err != nil {
+		return nil, err
+	}
+	tillerHost := fmt.Sprintf("127.0.0.1:%d", tillerTunnel.Local)
+	options := []helm.Option{helm.Host(tillerHost), helm.ConnectTimeout(300)}
+	return helm.NewClient(options...), nil
+}
+
+func configForContext(context string, kubeconfig string) (*rest.Config, error) {
+	config, err := kube.GetConfig(context, kubeconfig).ClientConfig()
+	if err != nil {
+		return nil, fmt.Errorf("could not get Kubernetes config for context %q: %s", context, err)
+	}
+	return config, nil
+}
+
+func getKubeClient(context string, kubeconfig string) (*rest.Config, kubernetes.Interface, error) {
+	config, err := configForContext(context, kubeconfig)
+	if err != nil {
+		return nil, nil, err
+	}
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, nil, fmt.Errorf("could not get Kubernetes client: %s", err)
+	}
+	return config, client, nil
+}
--- main.go
+++ main.go
@@ -1,15 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/h3poteto/helm/pkg/helm"
-	"github.com/h3poteto/helm/pkg/helm/portforwarder"
 	log "github.com/sirupsen/logrus"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	"k8s.io/helm/pkg/kube"
 )
 
 func main() {
@@ -18,43 +10,4 @@ func main() {
 		log.Fatal(err)
 	}
 	log.Info(client)
-}
-
-func NewClient() (*helm.Client, error) {
-	kubeconfig := os.Getenv("KUBECONFIG")
-	if len(kubeconfig) == 0 {
-		kubeconfig = "~/.kube/config"
-	}
-
-	config, client, err := getKubeClient("", kubeconfig)
-	if err != nil {
-		return nil, err
-	}
-	tillerTunnel, err := portforwarder.New("kube-system", client, config)
-	if err != nil {
-		return nil, err
-	}
-	tillerHost := fmt.Sprintf("127.0.0.1:%d", tillerTunnel.Local)
-	options := []helm.Option{helm.Host(tillerHost), helm.ConnectTimeout(300)}
-	return helm.NewClient(options...), nil
-}
-
-func configForContext(context string, kubeconfig string) (*rest.Config, error) {
-	config, err := kube.GetConfig(context, kubeconfig).ClientConfig()
-	if err != nil {
-		return nil, fmt.Errorf("could not get Kubernetes config for context %q: %s", context, err)
-	}
-	return config, nil
-}
-
-func getKubeClient(context string, kubeconfig string) (*rest.Config, kubernetes.Interface, error) {
-	config, err := configForContext(context, kubeconfig)
-	if err != nil {
-		return nil, nil, err
-	}
-	client, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return nil, nil, fmt.Errorf("could not get Kubernetes client: %s", err)
-	}
-	return config, client, nil
 }
